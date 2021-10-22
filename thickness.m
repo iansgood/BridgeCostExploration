05bccb03079188352f77fd4c68d5679407627860
Ian Good <72166495+iansgood@users.noreply.github.com>
2021-10-22 15:19:50 -0700

    Add files via upload
    renamed bridge thickness function and changed input format to that of a vector/

---
+++ thickness.m
@@ -0,0 +1,22 @@
+function [h_Str,h_Def] = thickness(tr,yeild,E)
+%{
+ HEIGHT calculates the minimum safe thickness for a beam using yeilding and deflection
+ INPUTS
+    deflection is the maximum allowed deflection for a fixed beam. [m]
+    P is the point load [N]
+    L is the beam length [m]
+    yeild is the yeild strength [MPa]
+    b is the beam width [m]
+    E is the Modulus of Elasticty [MPa]
+  OUTPUTS
+  h_Str is the height required for yeilding to not take place
+  h_def is the height required for the deflection criteria to be met
+%}
+
+p = tr.width.*tr.loadMax;
+m = p.*tr.length_eng/4;
+
+h_Str = sqrt(8.* p.*tr.length_eng ./ tr.width.*yeild/1E6); %in m
+h_Def = (p.*tr.length_eng.^3./(4*tr.width.*E/1E6.*tr.deflectionMax)).^1/3; %in m
+end
+
