61fa13b00d65975a88ae1cc25c31d96e06405e36
novalex98 <AlexanderNovok98@gmail.com>
2021-10-23 17:05:07 -0700

    Re-added titanium and ABS

--- main.m
+++ main.m
@@ -5,14 +5,22 @@ clear all; close all; clc;
 SS = struct('E',28,'yeild', 75,'density', 0.29,'cost',2); % in [psi*10^6] [psi*10^3], [lb/in^3] , [$/lb]
 AL = struct('E',10,'yeild', 40,'density', 0.1,'cost',4.5); % in [psi*10^6] [psi*10^3], [lb/in^3] , [$/lb]
 GFRP = struct('E',4,'yeild',45,'density',0.065,'cost',7); % in [psi*10^6] [psi*10^3], [lb/in^3] , [$/lb]
-%TI = struct('E',114000,'yeild',830, 'density',4428.78475,'cost',103.04); % in [MPa] [Mpa], [kg/m^3] , [$/kg]  (Ti6-Al4-V retrieved from Matweb.com, price from McMasterCarr Bar Stock)
-%ABS = struct('E',8200,'yeild',110, 'density',1240,'cost',9.65); % in [MPa] [Mpa], [kg/m^3] , [$/kg] (ABS-GF30 retrieved from efunda, price from German Plastics marketplace pasticker.de)
-%after having made these as structs, I think it might make more sense to
-%just use vectors for each of the different terms
-yeild = [SS.yeild AL.yeild GFRP.yeild]; %in psi*10^3% TI.yeild ABS.yeild]; %in MPa
-E = [SS.E AL.E GFRP.E];% in psi*10^6 TI.E ABS.E]; %in MPa
-density = [SS.density AL.density GFRP.density]; % in lb/in^3 TI.density ABS.density];
-cost = [SS.cost AL.cost GFRP.cost]; % in $/lb TI.cost ABS.cost]; % in $/kg
+TI = struct('E',16.5,'yeild',125, 'density',0.16,'cost',103.04); % in [psi*10^6] [psi*10^3], [lb/in^3] , [$/lb]  (Ti6-Al4-V retrieved from Matweb.com, price from McMasterCarr Bar Stock)
+% Titanium properties: Titanium Ti-6Al-4V (Grade 5), Annealed Bar from Matweb
+% Titanium price: https://www.mcmaster.com/9081K49/
+MPatomsi = 145/10^6; % Converts megapascals to mega-pounds per square inch
+MPatoksi = 0.145; % Converts megapascals to killopounds per square inch
+sgtolbpercubicinch = 0.03613; % converts specific gravity to pounds per cubic inch
+ABS = struct('E',6900*MPatomsi,'yeild',104*MPatoksi, 'density',1.29*sgtolbpercubicinch,'cost',4.38); % in [psi*10^6] [psi*10^3], [lb/in^3] , [$/lb] (ABS-GF30 retrieved from efunda, price from German Plastics marketplace pasticker.de)
+% For ABS low values of E and yield strength are taken as conservative estimates.
+% Yield strength is defined as compressive strength because the bridge will
+% experience primarily compressive load.
+% ABS properties: https://www.efunda.com/Materials/polymers/properties/polymer_datasheet.cfm?MajorID=ABS&MinorID=13
+% ABS price: https://plasticker.de/recybase/showdata_en.php?id=324711&returnto=%2Frecybase%2Fpms2_en.php%3Fkat%3DMahlgut%26mat%3DABS%26aog%3DA
+yeild = [SS.yeild AL.yeild GFRP.yeild TI.yeild ABS.yeild]; %in psi*10^3% 
+E = [SS.E AL.E GFRP.E TI.E ABS.E];% in psi*10^6 
+density = [SS.density AL.density GFRP.density TI.density ABS.density]; % in lb/in^3 
+cost = [SS.cost AL.cost GFRP.cost TI.cost ABS.cost]; % in $/lb 
 
 % Bridge Parameters %
 fttoinch = 12; % inches per foot
