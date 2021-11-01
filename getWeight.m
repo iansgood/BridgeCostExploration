8621795d27111c31f70cad4e69a22b8c6a2ec019
peytonwells7 <75403854+peytonwells7@users.noreply.github.com>
2021-10-31 23:01:54 -0700

    Problem 4 Weight Function

--- getWeight.m
+++ getWeight.m
@@ -1,6 +1,6 @@
 function [weight] = getWeight(A,L,p)
 
-% This function uses the area, length and density to get weight
+% This function uses the area, length and density to get weight, used in Problem 4
 
 weight = A*L*p;
 
